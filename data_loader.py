47345c2dd86bbddc0155db8aa9a0640ee08b689c
Therainbowmushroom <nikolaeva_ea04@mail.ru>
2025-09-23 13:29:13 +0300

    Delete data_loader.py

--- data_loader.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-file_id = "1zmNAMYzerjdrCZs51MmoWiHfvog-xdi3"  # ID файла на Google Drive
-file_url = f"https://drive.google.com/uc?id={file_id}"
-
-df_lung = pd.read_csv(file_url)
-
-df_lung.head(10)
-
