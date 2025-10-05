4cadb4b3855e93c8fdd65be306386b3dc40dec97
Therainbowmushroom <nikolaeva_ea04@mail.ru>
2025-10-05 21:19:54 +0300

    script for parsing website: https://www.samadm.ru/

---
+++ parse_example/parser.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+
+source = 'https://www.samadm.ru/'
+req = requests.get(source)
+req.raise_for_status()
+print(len(req.text))
+req.text
+
+req_soup = BeautifulSoup(req.text, 'html.parser')
+req_soup
+
+news = req_soup.select('div.news')
+
+dates = []
+text_news_list = []
+links = []
+
+for new in news:
+
+    date = new.select_one('time') if new else None
+    link_all = new.select_one('a') if new else None
+    text_news = link_all.text.strip() if link_all else None
+    link = link_all['href'] if link_all else None
+
+    dates.append(date)
+    text_news_list.append(text_news)
+    links.append(link)
+
+columns = ["date", "link", "text_news"]
+df_news = pd.DataFrame(list(zip(dates, links, text_news_list)), 
+                       columns=columns)
+print(df_news.head())
+
+df_news.to_csv('df_news_samara', index=False)
